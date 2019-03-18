5d6309b4b1b52f690e2fd069512fbd8b231c8f13
Niamul Arifin <niamul@gmail.com>
2019-03-18 00:58:59 -0400

    1st release of the project

---
+++ src/test/java/executionTest/t1.java
@@ -0,0 +1,13 @@
+package executionTest;
+
+import org.testng.annotations.Test;
+
+public class t1 {
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("Git-Jenkins build integration example to ArifinBoys");
+	}
+
+}
